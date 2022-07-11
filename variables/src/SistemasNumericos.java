8499264e34b941da5c95ed08c169be4cf95404ec
roberto <rpajueloferreira@gmail.com>
2022-07-11 18:37:26 +0200

    second commit

--- variables/src/SistemasNumericos.java
+++ variables/src/SistemasNumericos.java
@@ -1,26 +1,43 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class SistemasNumericos {
     public static void main(String[] args) {
-
-        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un numero decimal");
-        int numeroDecimal = Integer.parseInt(numeroStr);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero:");
+        String numeroStr = scanner.nextLine();
+        int numeroDecimal = 0;
+        try{
+            Integer.parseInt(numeroStr);
+        }catch(NumberFormatException e){
+            System.out.println("Error debe ingresar un numero entero");
+            main(args);
+            System.exit(0);
+        }
         System.out.println("numeroDecimal = " + numeroDecimal);
 
         String resutadoBinario = ("numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
         System.out.println(resutadoBinario);
+
         int numeroBinario = 0b111110100;
         System.out.println("numeroBinario = " + numeroBinario);
 
         String resultadoOctal = ("numero octal de " + numeroBinario + " = " + Integer.toOctalString(numeroBinario));
         System.out.println(resultadoOctal);
-        int numeroOctal = 0764;
+        int numeroOctal = 036;
         System.out.println("numeroOctal = " + numeroOctal);
 
         String resultadoHexadecimal = ("numero hexadecimal de " + numeroOctal + " = " + Integer.toHexString(numeroOctal));
         System.out.println(resultadoHexadecimal);
         int numeroHex = 0x1f4;
         System.out.println("numeroHex = " + numeroHex);
+
+        String mensaje = resutadoBinario;
+        mensaje += "\n" + resultadoOctal;
+        mensaje += "\n" + resultadoHexadecimal;
+
+        System.out.println(mensaje);
+
 
 
     }
